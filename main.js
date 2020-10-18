05969ac9697d31dabe464003fb0db25da12bc75a
jtvberg <jtvberg@gmail.com>
2020-10-18 18:02:50 -0500

    multi-service

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 // Imports and variable declarations
 const { app, BrowserWindow, ipcMain, BrowserView, session } = require('electron')
-const updater = require('./updater')
+const { data } = require('jquery')
+// const updater = require('./updater')
 
 // Enable Electron-Reload (dev only)
-// require('electron-reload')(__dirname)
+require('electron-reload')(__dirname)
 
 // Main window
 let win = null
+let view = null
 const createWindow = () => {
   // Check for updates after 3 seconds
   // setTimeout(updater, 3000)
@@ -18,34 +20,34 @@ const createWindow = () => {
     transparent: true,
     frame: false,
     hasShadow: false,
-    // userAgent: 'Chrome',
     webPreferences: {
       plugins: true,
-      nodeIntegration: false
+      nodeIntegration: true
     }
   })
 
   // HTML file to load into window
   win.loadFile('main.html')
 
-  // Show window when ready
-  // win.once('ready-to-show', () => {
-  //   win.show()
-  // })
-
   // Set initial state of always on top
   // win.setAlwaysOnTop(true, 'floating')
 
   // Open DevTools (dev only)
-  // win.webContents.openDevTools()
+  win.webContents.openDevTools()
 
-  const view = new BrowserView()
+  win.on('resize', function () {
+    wb = win.getBounds()
+    view.setBounds({ x: 0, y: 25, width: wb.width, height: wb.height - 25 })
+  })
+
+  view = new BrowserView()
   win.setBrowserView(view)
-  view.setBounds({ x: 0, y: 0, width: 800, height: 600 })
-  view.setAutoResize({ width: true, height: true })
+  view.setBounds({ x: 0, y: 25, width: 800, height: 575 })
   view.webContents.loadURL('https://www.netflix.com')
-  // view.webContents.loadURL('https://www.hulu.com', {userAgent: 'Chrome'})
-  // view.webContents.loadURL('https://www.amazon.com/gp/video/storefront/', {userAgent: 'Chrome'})
+  // view.webContents.loadURL('https://tv.youtube.com')
+  // view.webContents.loadURL('https://www.hulu.com')
+  // view.webContents.loadURL('https://www.amazon.com/gp/video/storefront/')
+  // view.webContents.loadURL('https://www.disneyplus.com/home')
 }
 
 app.commandLine.appendSwitch('no-verify-widevine-cdm')
@@ -84,27 +86,48 @@ app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit()
 })
 
-// // IPC channel for locking app on top
-// ipcMain.on('ontop-lock', function () {
-//   win.setAlwaysOnTop(true, 'floating')
-// })
+// IPC channel to change streaming service
+ipcMain.on('service-change', (e, data) => {
+  switch (data) {
+    case 'nf':
+      view.webContents.loadURL('https://www.netflix.com')
+      break
+    case 'yt':
+      view.webContents.loadURL('https://tv.youtube.com')
+      break
+    case 'hl':
+      view.webContents.loadURL('https://www.hulu.com')
+      break
+    case 'ap':
+      view.webContents.loadURL('https://www.amazon.com/gp/video/storefront/')
+      break
+    case 'dp':
+      view.webContents.loadURL('https://www.disneyplus.com/home')
+      break
+  }
+})
 
-// // IPC channel for unlocking app on top
-// ipcMain.on('ontop-unlock', function () {
-//   win.setAlwaysOnTop(false)
-// })
+// IPC channel for locking app on top
+ipcMain.on('ontop-lock', function () {
+  win.setAlwaysOnTop(true, 'floating')
+})
 
-// // IPC channel for maximizing window
-// ipcMain.on('win-max', () => {
-//   win.maximize()
-// })
+// IPC channel for unlocking app on top
+ipcMain.on('ontop-unlock', function () {
+  win.setAlwaysOnTop(false)
+})
 
-// // IPC channel for restoring window
-// ipcMain.on('win-restore', () => {
-//   win.unmaximize()
-// })
+// IPC channel for maximizing window
+ipcMain.on('win-max', () => {
+  win.maximize()
+})
 
-// // IPC channel for maximizing window
-// ipcMain.on('win-hide', () => {
-//   win.hide()
-// })
+// IPC channel for restoring window
+ipcMain.on('win-restore', () => {
+  win.unmaximize()
+})
+
+// IPC channel for maximizing window
+ipcMain.on('win-hide', () => {
+  win.hide()
+})
--- renderer.js
+++ renderer.js
@@ -1,3 +1,40 @@
 // Imports and variable declarations
-// const { ipcRenderer } = require('electron')
-// const $ = require('jquery')
+const { ipcRenderer, ipcMain } = require('electron')
+const $ = require('jquery')
+let winMax = false
+
+// Window max/restore on header double click
+function maxRestoreWindow () {
+  if (!winMax) {
+    ipcRenderer.send('win-max')
+    $('body').css('background-color', '#00000000')
+    $('.header-bar').addClass('header-bar-max')
+    winMax = true
+  } else {
+    ipcRenderer.send('win-restore')
+    winMax = false
+    $('body').css('background-color', '#161616be')
+    $('.header-bar').removeClass('header-bar-max')
+  }
+}
+
+// Toggle keep on top
+$('.ontop-button').on('click', function () {
+  if ($(this).hasClass('ontop-locked')) {
+    $(this).removeClass('ontop-locked').addClass('ontop-unlocked')
+    ipcRenderer.send('ontop-unlock')
+  } else {
+    $(this).removeClass('ontop-unlocked').addClass('ontop-locked')
+    ipcRenderer.send('ontop-lock')
+  }
+})
+
+// Header double-click handler
+$('.header-bar').on('dblclick', () => {
+  maxRestoreWindow()
+})
+
+// Service selector click handler
+$('.service-button').on('click', function () {
+  ipcRenderer.send('service-change', $(this).data('val'))
+})
