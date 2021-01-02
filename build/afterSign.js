f75eeddeae099a0dde0bc428803e59882dca35f5
jtvberg <jtvberg@gmail.com>
2021-01-02 15:34:49 -0600

    Move build scripts

---
+++ build/afterSign.js
@@ -0,0 +1,25 @@
+const { notarize } = require('electron-notarize')
+
+exports.default = function (context) {
+  // Skip if not mac build
+  if (process.platform === 'darwin') {
+    console.log('Notarizing')
+    // Get contex vars
+    const appName = context.packager.appInfo.productFilename
+    const appDir = context.appOutDir
+
+    // Notarize
+    return notarize({
+      appBundleId: 'com.jtvberg.streamdock',
+      appPath: `${appDir}/${appName}.app`,
+      appleId: process.env.appleId,
+      appleIdPassword: process.env.appleIdPassword
+    })
+  } else {
+    // VMP sign via EVS
+    const { execSync } = require('child_process')
+    console.log('VMP signing start')
+    execSync('py -3 -m castlabs_evs.vmp sign-pkg ./dist/win-unpacked')
+    console.log('VMP signing complete')
+  }
+}
