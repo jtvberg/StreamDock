49e23cb106452ede74823b58ea5328fe14a317ea
Joel Vandenberg <jtvberg@gmail.com>
2023-05-23 17:29:07 -0500

    api param break out

--- public/js/util/tmdb.js
+++ public/js/util/tmdb.js
@@ -69,66 +69,95 @@ async function apiPost(url, body) {
 }
 
 async function getRequestToken() {
-  const response = await apiGet(`${apiHost}/3/authentication/token/new?api_key=${getApiKey()}`)
+  const response = await apiGet(`${getHost()}/3/authentication/token/new?api_key=${getApiKey()}`)
   showResponse(response)
 }
 
 async function validateRequestToken() {
-  const response = await apiPost(`${apiHost}/3/authentication/token/validate_with_login?api_key=${getApiKey()}`, { username: getUsername(), password: getPassword(), request_token: requestToken })
+  const response = await apiPost(`${getHost()}/3/authentication/token/validate_with_login?api_key=${getApiKey()}`, { username: getUsername(), password: getPassword(), request_token: requestToken })
   showResponse(response)
 }
 
 async function getSessionId() {
-  const response = await apiGet(`${apiHost}/3/authentication/session/new?api_key=${getApiKey()}&request_token=${requestToken}`)
+  const response = await apiGet(`${getHost()}/3/authentication/session/new?api_key=${getApiKey()}&request_token=${requestToken}`)
   showResponse(response)
 }
 
 async function setFav(media_type, media_id, favorite) {
-  const response = await apiPost(`${apiHost}/3/account/${getUsername()}/favorite?api_key=${getApiKey()}&session_id=${sessionId}`, { media_type, media_id, favorite })
+  const response = await apiPost(`${getHost()}/3/account/${getUsername()}/favorite?api_key=${getApiKey()}&session_id=${sessionId}`, { media_type, media_id, favorite })
   showResponse(response)
 }
 
 async function getLists() {
-  const response = await apiGet(`${apiHost}/3/account/${getUsername()}/lists?api_key=${getApiKey()}&session_id=${sessionId}`)
+  const response = await apiGet(`${getHost()}/3/account/${getUsername()}/lists?api_key=${getApiKey()}&session_id=${sessionId}`)
   showResponse(response)
 }
 
 async function getList(id) {
-  const response = await apiGet(`${apiHost}/3/list/${id}?api_key=${getApiKey()}&session_id=${sessionId}`)
+  const response = await apiGet(`${getHost()}/3/list/${id}?api_key=${getApiKey()}&session_id=${sessionId}`)
   showResponse(response)
 }
 
 async function setList(name, description) {
-  const response = await apiPost(`${apiHost}/3/list?api_key=${getApiKey()}&session_id=${sessionId}`, { name, description, language: 'en' })
+  const response = await apiPost(`${getHost()}/3/list?api_key=${getApiKey()}&session_id=${sessionId}`, { name, description, language: 'en' })
   showResponse(response)
 }
 
 async function setListItem(list, media_id) {
-  const response = await apiPost(`${apiHost}/3/list/${list}/add_item?api_key=${getApiKey()}&session_id=${sessionId}`, { media_id })
+  const response = await apiPost(`${getHost()}/3/list/${list}/add_item?api_key=${getApiKey()}&session_id=${sessionId}`, { media_id })
   showResponse(response)
 }
 
 async function getFavs(type) {
-  const response = await apiGet(`${apiHost}/3/account/${getUsername()}/favorite/${type}?api_key=${getApiKey()}&session_id=${sessionId}`)
+  const response = await apiGet(`${getHost()}/3/account/${getUsername()}/favorite/${type}?api_key=${getApiKey()}&session_id=${sessionId}`)
   showResponse(response)
 }
 
 export async function searchTitle(term, page) {
-  return await apiGet(`${getHost()}/3/search/multi?api_key=${getApiKey()}&language=${getLangLoc()}&query=${encodeURIComponent(term)}&page=${page}&include_adult=false`)
+  const url = new URL(`${getHost()}/3/search/multi`)
+  url.searchParams.append('api_key', getApiKey())
+  url.searchParams.append('language', getLangLoc())
+  url.searchParams.append('query', term)
+  url.searchParams.append('page', page)
+  url.searchParams.append('include_adult', false)
+  return await apiGet(url)
 }
 
 export async function getTrendingTitles(time, page) {
-  return await apiGet(`${getHost()}/3/trending/all/${time}?api_key=${getApiKey()}&language=${getLangLoc()}&page=${page}&include_adult=false`)
+  const url = new URL(`${getHost()}/3/trending/all/${time}`)
+  url.searchParams.append('api_key', getApiKey())
+  url.searchParams.append('language', getLangLoc())
+  url.searchParams.append('page', page)
+  url.searchParams.append('include_adult', false)
+  return await apiGet(url)
 }
 
 export async function getDiscoveryTitles(media_type, page) {
-  return await apiGet(`${getHost()}/3/discover/${media_type}?api_key=${getApiKey()}&language=${getLangLoc()}&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&watch_region=${getLoc()}&with_watch_monetization_types=flatrate`)
+  const url = new URL(`${getHost()}/3/discover/${media_type}`)
+  url.searchParams.append('api_key', getApiKey())
+  url.searchParams.append('language', getLangLoc())
+  url.searchParams.append('sort_by', 'popularity.desc')
+  url.searchParams.append('include_adult', false)
+  url.searchParams.append('include_video', false)
+  url.searchParams.append('page', page)
+  url.searchParams.append('watch_region', getLoc())
+  url.searchParams.append('with_watch_monetization_types', 'flatrate')
+  return await apiGet(url)
 }
 
 export async function getRecommendedTitles(id, media_type, page) {
-  return await apiGet(`${getHost()}/3/${media_type}/${id}/recommendations?api_key=${getApiKey()}&language=${getLangLoc()}&page=${page}&include_adult=false`)
+  const url = new URL(`${getHost()}/3/${media_type}/${id}/recommendations`)
+  url.searchParams.append('api_key', getApiKey())
+  url.searchParams.append('language', getLangLoc())
+  url.searchParams.append('page', page)
+  url.searchParams.append('include_adult', false)
+  return await apiGet(url)
 }
 
 export async function getTitleDetails(id, media_type) {
-  return await apiGet(`${getHost()}/3/${media_type}/${id}?api_key=${getApiKey()}&append_to_response=credits,watch/providers,genres,release_dates,content_ratings`)
+  const url = new URL(`${getHost()}/3/${media_type}/${id}`)
+  url.searchParams.append('api_key', getApiKey())
+  url.searchParams.append('language', getLangLoc())
+  url.searchParams.append('append_to_response', 'credits,watch/providers,genres,release_dates,content_ratings')
+  return await apiGet(url)
 }
